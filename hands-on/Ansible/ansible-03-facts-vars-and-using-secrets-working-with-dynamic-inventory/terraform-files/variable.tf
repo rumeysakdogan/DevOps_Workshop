031858552ed9d918dd203b78fa15b0fa9fa0c98b
rumeysakdogan <dgn.rumeysaa@gmail.com>
2022-08-29 22:16:14 -0400

    adding terraform files

---
+++ hands-on/Ansible/ansible-03-facts-vars-and-using-secrets-working-with-dynamic-inventory/terraform-files/variable.tf
@@ -0,0 +1,10 @@
+//variable "aws_secret_key" {}
+//variable "aws_access_key" {}
+variable "region" {}
+variable "mykey" {}
+variable "tags" {}
+variable "myami" {
+  description = "3 amazon linux 2 ami"
+}
+variable "instancetype" {}
+variable "num" {}
